042069a530abe3ff51edcb5784178efde3c65fe1
sami <84959599+Samikhan5823@users.noreply.github.com>
2022-03-17 22:23:58 +0500

    change footer

--- src/Component/layout/Footer.js
+++ src/Component/layout/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer>
       <div className="footer">
-        All rights reserved, 2021 by{' '}
+        All rights reserved, 2022 by{' '}
         <span>
           {/* <img className="footer-logo" src={medifusionLOGO} alt="Footer Logo" /> */}
         </span>
