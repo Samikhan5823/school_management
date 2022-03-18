266946803dd6d43794740db963ea817ee41b86f8
sami <84959599+Samikhan5823@users.noreply.github.com>
2022-03-18 22:56:24 +0500

    dashboard setting

--- src/Component/SiderbarMenu/DashBoard/Dashboard.js
+++ src/Component/SiderbarMenu/DashBoard/Dashboard.js
@@ -7,7 +7,7 @@ import BatchPic from '../../../images/batch.png'
 import Chart from './Chart'
 const Dashboard = (props) => {
   return (
-    
+
     <div className="page-header" >
       <div className="row"  >
         <div className="page-header-content">
@@ -68,30 +68,28 @@ const Dashboard = (props) => {
         </div>
         <div className="content">
           <div className="row">
-            <div className="col-lg-12 col-xl-8">
-              <div className=" panel-heading">
+            <div className="col-xs-12 col-sm-8 col-xl-8">
               <ul className="news-heading">
-                  <li className="active">
-                    <a className="text-size-small text-uppercase">
-                  <h6 className="panel-title">Daily Attendance Overview </h6>
+                <li className="active">
+                  <a className="text-size-small text-uppercase">
+                    <h6 className="panel-title">Daily Attendance Overview </h6>
 
-                    </a>
-                  </li>
-                </ul>
-              </div>
+                  </a>
+                </li>
+              </ul>
 
               <div className=" panel-body">
                 {' '}
                 <Chart />{' '}
               </div>
             </div>
-            <div className="col-lg-12 col-xl-4 mb-sm-2">
-           
+            <div className="col-xs-12 col-sm-4 col-xl-4 mb-sm-2">
+
               <div
-                className=" panel panel-flat"
+                className=" panel panel-flat mt-5 mt-sm-0 " 
                 style={{ height: '30vh' }}
               >
-                <ul className="news-heading">
+                <ul className="news-heading ">
                   <li className="active">
                     <a className="text-size-small text-uppercase">
                       Notice Board
@@ -102,7 +100,7 @@ const Dashboard = (props) => {
                   <ul className="media-list">
                     <li className="media">
                       <div className="media-left">
-                   
+
                         <span className="badge ">f</span>
                       </div>
                       <div className="media-body">
@@ -122,9 +120,88 @@ const Dashboard = (props) => {
                   </ul>
                 </div>
               </div>
+
+
+
+
               <div
                 className=" panel panel-flat"
-                style={{ height: '30vh'}}
+              // style={{ height: '30vh' }}
+              >
+                <ul className="news-heading">
+                  <li className="active">
+                    <a className="text-size-small text-uppercase">
+                      Fee Collection
+                    </a>
+                  </li>
+                </ul>
+                <div className="tab-content">
+                  <ul className="media-list">
+                    <li className="media">
+                      {/* <div className="media-left">
+
+                        <span className="badge ">f</span>
+                      </div> */}
+                      <div className="media-body">
+
+
+                        <span>
+                          <i className="media-calender"> 12-10-2020</i>
+                        </span>
+
+                        <div className=" row text-center d-flex m-5">
+                          <div className="col-md-4">
+                            <div className="content-group">
+                              <h6 className="text-semibold-no-margin">
+                                <i className="icon-cash3-position-left-text-slate"></i>
+                                0
+                              </h6>
+                              <span className="text-muted text-size-small">
+                                {' '}
+                                Amount
+                              </span>
+                            </div>
+
+                          </div>
+                          <div className="col-md-4">
+                            <div className="content-group">
+                              <h6 className="text-semibold-no-margin">
+                                <i className="icon-cash3-position-left-text-slate"></i>
+                                34
+                              </h6>
+                              <span className="text-muted text-size-small">
+                                {' '}
+                                Discount
+                              </span>
+                            </div>
+                          </div>
+                          <div className="col-md-4">
+                            <div className="content-group">
+                              <h6 className="text-semibold-no-margin">
+                                <i className="icon-cash3-position-left-text-slate"></i>
+                                30
+                              </h6>
+                              <span className="text-muted text-size-small">
+                                {' '}
+                                Fine
+                              </span>
+                            </div>
+                          </div>
+                          <hr />
+                        </div>
+                        {/* <span className="display-text">
+                          <marquee behavior="scroll" direction="left">
+                            Tomorrow Result will be decleared ...{' '}
+                          </marquee>
+                        </span> */}
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              {/* <div
+                className=" panel panel-flat"
+                style={{ height: '30vh' }}
               >
                 <ul className="news-heading">
                   <li className="active">
@@ -133,12 +210,12 @@ const Dashboard = (props) => {
                     </a>
                   </li>
                 </ul>
-                <div >
-                <div className="heading-elements" >
-                      <span>
-                            <i className="media-calender"style={{marginTop:'100px'}}> 12-10-2020</i>
-                          </span>
-                </div>
+                <div  className="media-body">
+                  <div className="heading-elements" >
+                    <span>
+                      <i className="media-calender" style={{ marginTop: '100px' }}> 12-10-2020</i>
+                    </span>
+                  </div>
                   <div className=" row text-center d-flex m-5">
                     <div className="col-md-4">
                       <div className="content-group">
@@ -151,7 +228,7 @@ const Dashboard = (props) => {
                           Amount
                         </span>
                       </div>
-                 
+
                     </div>
                     <div className="col-md-4">
                       <div className="content-group">
@@ -177,18 +254,18 @@ const Dashboard = (props) => {
                         </span>
                       </div>
                     </div>
-                    <hr/>
+                    <hr />
                   </div>
-                </div>
-             
-              </div>
+                </div> */}
+
+              {/* </div> */}
 
 
 
 
 
             </div>
-           
+
           </div>
         </div>
       </div>
